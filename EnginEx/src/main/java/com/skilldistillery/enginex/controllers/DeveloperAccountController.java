d1da5b876b31fcdf3d5eaf1037e31a82e5611909
GarrettPi <86500870+GarrettPi@users.noreply.github.com>
2021-10-26 18:02:08 -0400

    Angular Stub

--- EnginEx/src/main/java/com/skilldistillery/enginex/controllers/DeveloperAccountController.java
+++ EnginEx/src/main/java/com/skilldistillery/enginex/controllers/DeveloperAccountController.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.enginex.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,11 @@ import com.skilldistillery.enginex.services.DeveloperAccountService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4301"})
 public class DeveloperAccountController {
 	
 	@Autowired
-	private DeveloperAccountService devrServ;
+	private DeveloperAccountService devServ;
 	
 	@GetMapping("ping")
 	public String ping() {
