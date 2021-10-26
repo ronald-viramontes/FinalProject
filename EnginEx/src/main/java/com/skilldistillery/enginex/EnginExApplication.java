325d572b8285e63bc88562ba40d95cdbcfd45201
jttweedy11 <87153150+jttweedy11@users.noreply.github.com>
2021-10-26 14:54:11 -0500

    create Rest project

---
+++ EnginEx/src/main/java/com/skilldistillery/enginex/EnginExApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.enginex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnginExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnginExApplication.class, args);
+	}
+
+}
---
+++ EnginEx/src/test/java/com/skilldistillery/enginex/EnginExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.enginex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnginExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
