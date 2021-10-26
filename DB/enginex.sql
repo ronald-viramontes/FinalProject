6c93d1a105cac5746aa92c4f7741ca24cc710615
ronald-viramontes <ronald.viramontes@gmail.com>
2021-10-26 19:53:19 -0400

    updated schema

--- DB/enginex.sql
+++ DB/enginex.sql
@@ -17,23 +17,46 @@ SHOW WARNINGS;
 USE `enginexdb` ;
 
 -- -----------------------------------------------------
--- Table `developer_account`
+-- Table `user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `developer_account` ;
+DROP TABLE IF EXISTS `user` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `developer_account` (
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
+  `role` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `developer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `developer` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `developer` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(250) NOT NULL,
   `last_name` VARCHAR(250) NOT NULL,
   `email` VARCHAR(250) NOT NULL,
-  `phone_number` VARCHAR(10) NULL,
-  `username` VARCHAR(250) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `account_active` TINYINT NOT NULL DEFAULT 1,
-  `image_url` TEXT NULL,
-  PRIMARY KEY (`id`))
+  `phone_number` VARCHAR(10) NULL DEFAULT NULL,
+  `image_url` TEXT NULL DEFAULT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_developer_account_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_developer_account_user1_idx` ON `developer` (`user_id` ASC);
 
 SHOW WARNINGS;
 
@@ -67,44 +90,50 @@ ENGINE = InnoDB;
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `client_account`
+-- Table `client`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `client_account` ;
+DROP TABLE IF EXISTS `client` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `client_account` (
+CREATE TABLE IF NOT EXISTS `client` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(250) NOT NULL,
   `last_name` VARCHAR(250) NOT NULL,
-  `phone_number` VARCHAR(10) NULL,
+  `phone_number` VARCHAR(10) NULL DEFAULT NULL,
   `email` VARCHAR(250) NOT NULL,
-  `username` VARCHAR(250) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `account_active` TINYINT NOT NULL DEFAULT 1,
   `image_url` TEXT NULL,
-  `company_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `company_id`),
+  `company_id` INT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_client_account_company1`
     FOREIGN KEY (`company_id`)
     REFERENCES `company` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_client_account_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SHOW WARNINGS;
-CREATE INDEX `fk_client_account_company1_idx` ON `client_account` (`company_id` ASC);
+CREATE INDEX `fk_client_account_company1_idx` ON `client` (`company_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `fk_client_account_user1_idx` ON `client` (`user_id` ASC);
 
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `status`
+-- Table `job_status`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `status` ;
+DROP TABLE IF EXISTS `job_status` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `status` (
+CREATE TABLE IF NOT EXISTS `job_status` (
   `id` INT NOT NULL,
-  `name` VARCHAR(255) NULL,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -127,8 +156,8 @@ CREATE TABLE IF NOT EXISTS `job_post` (
   `date_closed` DATE NULL,
   `job_type_id` INT NOT NULL,
   `client_account_id` INT NOT NULL,
-  `status_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `job_type_id`, `client_account_id`, `status_id`),
+  `job_status_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_job_post_job_type1`
     FOREIGN KEY (`job_type_id`)
     REFERENCES `job_type` (`id`)
@@ -136,12 +165,12 @@ CREATE TABLE IF NOT EXISTS `job_post` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_job_post_client_account1`
     FOREIGN KEY (`client_account_id`)
-    REFERENCES `client_account` (`id`)
+    REFERENCES `client` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_job_post_status1`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `status` (`id`)
+    FOREIGN KEY (`job_status_id`)
+    REFERENCES `job_status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -153,7 +182,7 @@ SHOW WARNINGS;
 CREATE INDEX `fk_job_post_client_account1_idx` ON `job_post` (`client_account_id` ASC);
 
 SHOW WARNINGS;
-CREATE INDEX `fk_job_post_status1_idx` ON `job_post` (`status_id` ASC);
+CREATE INDEX `fk_job_post_status1_idx` ON `job_post` (`job_status_id` ASC);
 
 SHOW WARNINGS;
 
@@ -165,15 +194,15 @@ DROP TABLE IF EXISTS `developer_education` ;
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `developer_education` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `education_type` TEXT NULL,
+  `education_type` TEXT NULL DEFAULT NULL,
   `institution_name` TEXT NOT NULL,
   `degree_certificate_name` TEXT NOT NULL,
   `complete_date` DATE NOT NULL,
   `developer_account_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `developer_account_id`),
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_developer_education_developer_account1`
     FOREIGN KEY (`developer_account_id`)
-    REFERENCES `developer_account` (`id`)
+    REFERENCES `developer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -191,15 +220,15 @@ DROP TABLE IF EXISTS `work_experience` ;
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `work_experience` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `job_title` VARCHAR(250) NULL,
-  `company_name` TEXT NULL,
+  `job_title` VARCHAR(250) NULL DEFAULT NULL,
+  `company_name` TEXT NULL DEFAULT NULL,
   `start_date` DATE NULL,
   `end_date` DATE NULL,
   `developer_account_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `developer_account_id`),
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_work_experience_developer_account1`
     FOREIGN KEY (`developer_account_id`)
-    REFERENCES `developer_account` (`id`)
+    REFERENCES `developer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -217,13 +246,13 @@ DROP TABLE IF EXISTS `developer_skill` ;
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `developer_skill` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `skill_title` VARCHAR(400) NULL,
-  `skill_level` VARCHAR(250) NULL,
+  `skill_title` VARCHAR(400) NULL DEFAULT NULL,
+  `skill_level` VARCHAR(250) NULL DEFAULT NULL,
   `developer_account_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `developer_account_id`),
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_developer_skills_developer_account1`
     FOREIGN KEY (`developer_account_id`)
-    REFERENCES `developer_account` (`id`)
+    REFERENCES `developer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -242,12 +271,12 @@ SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `job_application` (
   `id` INT NOT NULL,
   `application_approval` TINYINT NULL,
-  `application_status` VARCHAR(45) NULL,
+  `application_status` VARCHAR(45) NULL DEFAULT NULL,
   `application_date` DATE NULL,
   `job_post_id` INT NOT NULL,
   `developer_account_id` INT NOT NULL,
   `decision_date` DATE NULL,
-  PRIMARY KEY (`id`, `job_post_id`, `developer_account_id`),
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_job_application_status_job_post1`
     FOREIGN KEY (`job_post_id`)
     REFERENCES `job_post` (`id`)
@@ -255,7 +284,7 @@ CREATE TABLE IF NOT EXISTS `job_application` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_job_application_developer_account1`
     FOREIGN KEY (`developer_account_id`)
-    REFERENCES `developer_account` (`id`)
+    REFERENCES `developer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -279,34 +308,10 @@ CREATE TABLE IF NOT EXISTS `job_detail` (
   `start_date` DATE NULL,
   `finish_date` DATE NULL,
   `job_rating` INT NULL,
-  `job_rating_comment` TEXT NULL,
-  `job_post_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `job_post_id`),
-  CONSTRAINT `fk_job_details_job_post1`
-    FOREIGN KEY (`job_post_id`)
-    REFERENCES `job_post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-SHOW WARNINGS;
-CREATE INDEX `fk_job_details_job_post1_idx` ON `job_detail` (`job_post_id` ASC);
-
-SHOW WARNINGS;
-
--- -----------------------------------------------------
--- Table `developer_rating`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `developer_rating` ;
-
-SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `developer_rating` (
-  `id` INT NOT NULL,
-  `rating` INT NULL,
-  `comments` TEXT NULL,
+  `job_rating_comment` TEXT NULL DEFAULT NULL,
   `job_application_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `job_application_id`),
-  CONSTRAINT `fk_developer_rating_job_application1`
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_job_detail_job_application1`
     FOREIGN KEY (`job_application_id`)
     REFERENCES `job_application` (`id`)
     ON DELETE NO ACTION
@@ -314,7 +319,40 @@ CREATE TABLE IF NOT EXISTS `developer_rating` (
 ENGINE = InnoDB;
 
 SHOW WARNINGS;
-CREATE INDEX `fk_developer_rating_job_application1_idx` ON `developer_rating` (`job_application_id` ASC);
+CREATE INDEX `fk_job_detail_job_application1_idx` ON `job_detail` (`job_application_id` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `job_application_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `job_application_comment` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `job_application_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `comment` TEXT NULL DEFAULT NULL,
+  `comment_date` DATE NULL,
+  `job_application_id` INT NOT NULL,
+  `in_reply_to_comment_id` INT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_job_application_comment_job_application1`
+    FOREIGN KEY (`job_application_id`)
+    REFERENCES `job_application` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_job_application_comment_job_application_comment1`
+    FOREIGN KEY (`in_reply_to_comment_id`)
+    REFERENCES `job_application_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_job_application_comment_job_application1_idx` ON `job_application_comment` (`job_application_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `fk_job_application_comment_job_application_comment1_idx` ON `job_application_comment` (`in_reply_to_comment_id` ASC);
 
 SHOW WARNINGS;
 SET SQL_MODE = '';
@@ -331,11 +369,21 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `developer_account`
+-- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `enginexdb`;
-INSERT INTO `developer_account` (`id`, `first_name`, `last_name`, `email`, `phone_number`, `username`, `password`, `account_active`, `image_url`) VALUES (1, 'Ron', 'Viramontes', 'example@email.com', '5551215555', 'ron', '123', 0, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'rodfed', '123', 1, 'admin');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `developer`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `enginexdb`;
+INSERT INTO `developer` (`id`, `first_name`, `last_name`, `email`, `phone_number`, `image_url`, `user_id`) VALUES (1, 'Ron', 'Viramontes', 'example@email.com', '5551215555', 'https://i.natgeofe.com/k/093c14b4-978e-41f7-b1aa-3aff5d1c608a/gray-wolf-closeup_2x3.jpg', 1);
 
 COMMIT;
 
