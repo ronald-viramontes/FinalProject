9a6e9db693994cf27f1a0be237d35cb91668edcd
ronald-viramontes <ronald.viramontes@gmail.com>
2021-10-27 11:41:28 -0400

    entity mapping

--- JPAEnginEx/src/main/java/com/skilldistillery/enginex/entities/Client.java
+++ JPAEnginEx/src/main/java/com/skilldistillery/enginex/entities/Client.java
@@ -40,72 +40,66 @@ public class Client {
 	@JoinColumn(name="company_id")
 	private Company company;
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
 
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-
 	public String getImageUrl() {
 		return imageUrl;
 	}
-
 
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
 
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -119,12 +113,14 @@ public class Client {
 		return id == other.id;
 	}
 
-
 	@Override
 	public String toString() {
-		return "DeveloperAccount [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phoneNumber=" + phoneNumber
-				+ ", imageUrl=" + imageUrl + "]";
+		return "Client [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", imageUrl=" + imageUrl + ", company=" + company + "]";
 	}
+
+
+	
 	
 	
 	
