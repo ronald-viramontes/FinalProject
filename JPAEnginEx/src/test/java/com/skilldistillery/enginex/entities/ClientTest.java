1f053c2e2897c0c09db416c743857a65cb68b4bf
ronald-viramontes <ronald.viramontes@gmail.com>
2021-10-27 12:45:10 -0400

    user, client entity mapping tests

--- JPAEnginEx/src/test/java/com/skilldistillery/enginex/entities/ClientTest.java
+++ JPAEnginEx/src/test/java/com/skilldistillery/enginex/entities/ClientTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.enginex.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,44 +11,57 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
-	
-	private static EntityManagerFactory emf;
+class ClientTest {
+	public static EntityManagerFactory emf;
 	private EntityManager em;
-	private User user;
-
+	private Client client;
+	
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		emf = Persistence.createEntityManagerFactory("JPAEnginEx");
-
+		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		emf.close();
+		
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
 		em = emf.createEntityManager();
-		user = em.find(User.class, 1);
+		client = em.find(Client.class, 1);
+		
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		em.close();
-		user = null;
+		client = null;
+		
 	}
 
 	@Test
+	@DisplayName("test entity mappings for Developer")
 	void test() {
-		assertNotNull(user);
-		assertEquals("rodfed", user.getUsername());
-		
-		
-		
+		assertNotNull(client);
+		assertEquals("Don", client.getFirstName());
+	}
+	
+	@Test
+	@DisplayName("test relationship mappings on Developer")
+	void test2() {
+		assertEquals("Don", client.getFirstName());
+		assertEquals("Johnson", client.getLastName());
+		assertEquals("Hollywood", client.getCompany().getCompanyName());
+		assertEquals("donjohn", client.getUser().getUsername());
+	
 	}
 
+	
 }
