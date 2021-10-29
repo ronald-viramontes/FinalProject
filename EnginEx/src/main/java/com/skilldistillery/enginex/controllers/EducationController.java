b361b2fd068cf0221cf73c4d041b3559f15df75a
GarrettPi <86500870+GarrettPi@users.noreply.github.com>
2021-10-29 16:06:43 -0400

    Education controller, service, repo finished

---
+++ EnginEx/src/main/java/com/skilldistillery/enginex/controllers/EducationController.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.enginex.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.enginex.entities.DeveloperEducation;
+import com.skilldistillery.enginex.services.EducationService;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4300" })
+public class EducationController {
+
+	@Autowired
+	EducationService edSvc;
+	
+	@GetMapping("educations/{dId}")
+	public List<DeveloperEducation> getByDevId(HttpServletRequest req, HttpServletResponse res, Principal principal, @PathVariable int dId){
+		return edSvc.findByDevId(dId, principal.getName());
+	}
+	
+	@PostMapping("educations/{dId}")
+	public DeveloperEducation create(HttpServletRequest req, HttpServletResponse res, Principal principal, @RequestBody DeveloperEducation edu, @PathVariable int dId) {
+		return edSvc.create(dId, edu, principal.getName());
+	}
+	
+	@PutMapping("educations/{dId}/{eId}")
+	public DeveloperEducation edit(HttpServletRequest req, HttpServletResponse res, Principal principal, @RequestBody DeveloperEducation edu, @PathVariable int dId, @PathVariable int eId) {
+		return edSvc.edit(dId, edu, principal.getName(), eId);
+	}
+	
+	
+	@DeleteMapping("educations/{dId}/{eId}")
+	public void delete(HttpServletRequest req, HttpServletResponse res, Principal principal, @PathVariable int dId, @PathVariable int eId) {
+		edSvc.delete(dId, principal.getName(), eId);
+	}
+}
+ 
---
+++ EnginEx/src/main/java/com/skilldistillery/enginex/services/EducationService.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.enginex.services;
+
+import java.util.List;
+
+import com.skilldistillery.enginex.entities.DeveloperEducation;
+
+public interface EducationService {
+
+	List<DeveloperEducation> findByDevId(int devId, String username);
+	DeveloperEducation create(int devId, DeveloperEducation edu, String username);
+	DeveloperEducation edit(int devId, DeveloperEducation edu, String username, int eduId);
+	void delete(int devId, String username, int eduId);
+}
