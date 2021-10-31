6076e04d320fd0548a39c8296e155994b95e12df
jttweedy11 <87153150+jttweedy11@users.noreply.github.com>
2021-10-31 18:25:53 -0500

    Updates to job post.
    Updates to job post, service work for status, types, and clientId.

--- ngEnginEx/src/app/services/job-post.service.ts
+++ ngEnginEx/src/app/services/job-post.service.ts
@@ -3,22 +3,42 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { JobPost } from '../models/job-post';
+import { JobStatus } from '../models/job-status';
+import { JobType } from '../models/job-type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JobPostService {
   private baseUrl = 'http://localhost:8091/';
-  private url = this.baseUrl + 'api/jobs';
+  private jobsUrl = this.baseUrl + 'api/jobs';
+  private typeUrl = this.baseUrl + 'api/jobstatus';
 
   constructor(private http: HttpClient) {}
 
   index(): Observable<JobPost[]> {
     console.log('In call to DB.');
-    return this.http.get<JobPost[]>(this.url).pipe(
+    return this.http.get<JobPost[]>(this.jobsUrl).pipe(
       catchError((err: any) => {
         console.log(err);
-        return throwError('Bad');
+        return throwError('Bad Post Request');
+      })
+    );
+  }
+  create(jobPost: JobPost) {
+    return this.http.post(this.jobsUrl, jobPost).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('creating a Job Post was not successful.');
+      })
+    );
+  }
+  indexStatus(): Observable<JobStatus[]> {
+    console.log('in call to type DB');
+    return this.http.get<JobStatus[]>(this.typeUrl).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('Bad Type Request');
       })
     );
   }
