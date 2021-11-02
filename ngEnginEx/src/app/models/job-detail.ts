605dc841db7f857013f2d3bf6fc3c70b32e37f2d
ronald-viramontes <ronald.viramontes@gmail.com>
2021-11-02 00:41:45 -0400

    Update job-detail.ts

--- ngEnginEx/src/app/models/job-detail.ts
+++ ngEnginEx/src/app/models/job-detail.ts
@@ -6,21 +6,18 @@ export class JobDetail {
   finishDate: string;
   rating: number;
   comment: string;
-  jobApplication: JobApplication;
 
   constructor(
     id: number = 0,
     startDate: string = '',
     finishDate: string = '',
     rating: number = 0,
-    comment: string = '',
-    jobApplication = new JobApplication()
+    comment: string = ''
   ) {
     this.id = id;
     this.startDate = startDate;
     this.finishDate = finishDate;
     this.rating = rating;
     this.comment = comment;
-    this.jobApplication = jobApplication;
   }
 }
