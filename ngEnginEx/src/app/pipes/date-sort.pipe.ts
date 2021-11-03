9aba028c97d20b92265ae52ff883ce13496b52f6
jttweedy11 <87153150+jttweedy11@users.noreply.github.com>
2021-11-03 01:16:30 -0500

    Updates to Job Search Results
    Updates to wrap in card and styling.

--- ngEnginEx/src/app/pipes/date-sort.pipe.ts
+++ ngEnginEx/src/app/pipes/date-sort.pipe.ts
@@ -6,9 +6,11 @@ import { JobPost } from '../models/job-post';
 })
 export class DateSortPipe implements PipeTransform {
 
-  transform(jobPosts: JobPost[], args?: any): JobPost[] {
+  transform(jobPosts: JobPost[], returnNum: number): JobPost[] {
     let results: JobPost[] = [];
     results = jobPosts.sort((a, b) => new Date(a.datePosted).getDate() - new Date(b.datePosted).getDate());
+    let len = results.length;
+    results.splice(returnNum,(len-returnNum));
     return results;
   }
 }
