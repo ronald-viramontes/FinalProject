096544cc45d32dcbc31f8867339ad0c63282065f
GarrettPi <86500870+GarrettPi@users.noreply.github.com>
2021-11-03 13:16:29 -0400

    fixed date sorting

--- ngEnginEx/src/app/pipes/date-sort.pipe.ts
+++ ngEnginEx/src/app/pipes/date-sort.pipe.ts
@@ -8,7 +8,7 @@ export class DateSortPipe implements PipeTransform {
 
   transform(jobPosts: JobPost[], returnNum: number): JobPost[] {
     let results: JobPost[] = [];
-    results = jobPosts.sort((a, b) => new Date(a.datePosted).getDate() - new Date(b.datePosted).getDate());
+    results = jobPosts.sort((a, b) => new Date(Date.parse(b.datePosted)).valueOf() - new Date(Date.parse(a.datePosted)).valueOf());
     let len = results.length;
     results.splice(returnNum,(len-returnNum));
     return results;
