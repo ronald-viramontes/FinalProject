c4343d4b9e8e8649226feefe358494cbaa92d95a
ronald-viramontes <ronald.viramontes@gmail.com>
2021-11-03 11:35:17 -0400

    styling

--- ngEnginEx/src/app/app.component.ts
+++ ngEnginEx/src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'ngEnginEx';
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-  loggedIn(){
+  loggedIn() {
     return this.authService.checkLogin();
   }
 }
